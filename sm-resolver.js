bf4d3e2fd7d6245efff5675d51af4507eadbc332
HaydenCai-Arkade <internship@arkade.com.au>
2020-09-18 13:09:50 +1000

    added slice machines

---
+++ sm-resolver.js
@@ -0,0 +1,14 @@
+import * as EssentialSlices from 'essential-slices/src/slices'
+
+const __allSlices = {  ...EssentialSlices, }
+
+const NotFound = ({ sliceName }) => {
+	console.log(`[sm - resolver] component "${sliceName}" not found.`)
+	return process.env.NODE_ENV !== 'production' ? <p>component "{sliceName}" not found.</p> : <div />
+}
+
+export default ({ sliceName, i }) => {
+	return __allSlices[sliceName] ? __allSlices[sliceName] : () => <NotFound sliceName={sliceName} />
+}
+
+  
